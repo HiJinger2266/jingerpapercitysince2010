311a28b061bcad388dc955597dff91193f5ede73
鍾淯全 <yeayeajinger2266@gmail.com>
2021-09-06 21:57:07 +0800

    new haha

---
+++ assets/php/test_header.php
@@ -0,0 +1,49 @@
+<!--DOCTYPE html-->
+<html lang="zh-Hant-TW">
+
+<head>
+    <title>紙城市 | JingerPaperCitySince2010</title>
+    <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="./test_header_style.css" />
+</head>
+
+<body>
+    <header>
+        <div class="menubar">
+            <div class="logo_button">
+                <a href="./index.html"><img src="./header_logo.png" class="main_logo" /></a>
+            </div>
+            <div class="links_buttons">
+                <div class="try_to_set_center"></div>
+                <label for="toggle" class="menu_button"><img src="./assets/php/menu_black_24dp.svg"></label>
+                <!--製造那個按鈕-->
+                <input type="checkbox" id="toggle" class="infact_no_checkbox" />
+                <ul class="main_navigation">
+                    <li><a href="./Homepage.html">Home</a></li>
+                    <li><a href="./test_header.html">About</a></li>
+                    <li><a href="#history">History</a></li>
+                    <li><a href="#station">Stations</a></li>
+                </ul>
+                <div class="try_to_set_center"></div>
+            </div>
+        </div>
+    </header>
+    <div>
+
+
+        <h1>來個h1的字</h1>
+        <h2>來個h2的字</h2>
+        <h3>來個h3的字</h3>
+        <h4>來個h4的字</h4>
+        <h5>來個h5的字</h5>
+        <h6>來個h6的字</h6>
+        <h1>來個h1的字</h1>
+        <h2>來個h2的字</h2>
+        <h3>來個h3的字</h3>
+        <h4>來個h4的字</h4>
+        <h5>來個h5的字</h5>
+        <h6>來個h6的字</h6>
+    </div>
+</body>
+
+</html>
